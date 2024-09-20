0d2ce1e9a2bb6ce05b96621e1c6cccb4a6ef501a
teeranon <teeranonn124@gmail.com>
2024-09-21 05:03:48 +0700

    lastmodify

--- A1.py
+++ A1.py
@@ -1,1 +1,10 @@
-print("hi")
+score=[]
+for i in range(5):
+    x= [int(x) for x in input("Enter_Score :").split()]
+    print(x)
+    Sum_score=sum(x)
+    print(Sum_score)
+    score.append(Sum_score)
+print(score.index(max(score))+1,max(score))
+
+
